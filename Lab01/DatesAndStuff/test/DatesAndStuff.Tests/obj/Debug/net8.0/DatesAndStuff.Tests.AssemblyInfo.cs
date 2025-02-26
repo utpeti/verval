f1c3f7730a90ac26ec00a7785c5630c933c56e92
Korpos Botond <korposb@gmail.com>
2025-02-26 18:53:31 +0200

    some init s...

--- Lab01/DatesAndStuff/test/DatesAndStuff.Tests/obj/Debug/net8.0/DatesAndStuff.Tests.AssemblyInfo.cs
+++ Lab01/DatesAndStuff/test/DatesAndStuff.Tests/obj/Debug/net8.0/DatesAndStuff.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatesAndStuff.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9dace010cfe8ba4c2abcb2452e4735d3d578f8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatesAndStuff.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatesAndStuff.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
