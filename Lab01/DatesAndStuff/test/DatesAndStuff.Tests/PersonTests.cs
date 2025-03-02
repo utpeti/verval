86c079728cb349136243630f9a41b2812db76aef
Korpos Botond <korposb@gmail.com>
2025-03-02 18:03:27 +0200

    Second task done

--- Lab01/DatesAndStuff/test/DatesAndStuff.Tests/PersonTests.cs
+++ Lab01/DatesAndStuff/test/DatesAndStuff.Tests/PersonTests.cs
@@ -18,65 +18,71 @@ public class PersonTests
 
     }
 
-    [Test]
-    public void GotMerried_First_NameShouldChange()
+    private class MerriedTests : PersonTests
     {
-        // Arrange
-        string newName = "Test-Eleso Pista";
-        double salaryBeforeMarriage = sut.Salary;
-        var beforeChanges = Person.Clone(sut);
+        [Test]
+        public void GotMerried_First_NameShouldChange()
+        {
+            // Arrange
+            string newName = "Test-Eleso Pista";
+            double salaryBeforeMarriage = sut.Salary;
+            var beforeChanges = Person.Clone(sut);
 
-        // Act
-        sut.GotMarried(newName);
+            // Act
+            sut.GotMarried(newName);
 
-        // Assert
-        Assert.That(sut.Name, Is.EqualTo(newName)); // act = exp
+            // Assert
+            Assert.That(sut.Name, Is.EqualTo(newName)); // act = exp
 
-        sut.Name.Should().Be(newName);
-        sut.Should().BeEquivalentTo(beforeChanges, o => o.Excluding(p => p.Name));
+            sut.Name.Should().Be(newName);
+            sut.Should().BeEquivalentTo(beforeChanges, o => o.Excluding(p => p.Name));
 
-        //sut.Salary.Should().Be(salaryBeforeMarriage);
+            //sut.Salary.Should().Be(salaryBeforeMarriage);
 
-        //Assert.AreEqual(newName, sut.Name); // = (exp, act)
-        //Assert.AreEqual(salaryBeforeMarriage, sut.Salary);
+            //Assert.AreEqual(newName, sut.Name); // = (exp, act)
+            //Assert.AreEqual(salaryBeforeMarriage, sut.Salary);
+        }
+
+        [Test]
+        public void GotMerried_Second_ShouldFail()
+        {
+            // Arrange
+            string newName = "Test-Eleso-Felallo Pista";
+            sut.GotMarried("");
+
+            // Act
+            var task = Task.Run(() => sut.GotMarried(""));
+            try { task.Wait(); } catch { }
+
+            // Assert
+            Assert.IsTrue(task.IsFaulted);
+        }
     }
 
-    [Test]
-    public void GotMerried_Second_ShouldFail()
+    private class SalaryTests
     {
-        // Arrange
-        string newName = "Test-Eleso-Felallo Pista";
-        sut.GotMarried("");
+        [Test]
+        public void IncreaseSalary_PositiveIncrease_ShouldIncrease()
+        {
+            throw new NotImplementedException();
+        }
 
-        // Act
-        var task = Task.Run(() => sut.GotMarried(""));
-        try { task.Wait(); } catch { }
+        [Test]
+        public void IncreaseSalary_ZeroPercentIncrease_ShouldNotChange()
+        {
+            throw new NotImplementedException();
+        }
 
-        // Assert
-        Assert.IsTrue(task.IsFaulted);
-    }
+        [Test]
+        public void IncreaseSalary_NegativeIncrease_ShouldDecrease()
+        {
+            throw new NotImplementedException();
+        }
 
-    [Test]
-    public void IncreaseSalary_PositiveIncrease_ShouldIncrease()
-    {
-        throw new NotImplementedException();
-    }
-
-    [Test]
-    public void IncreaseSalary_ZeroPercentIncrease_ShouldNotChange()
-    {
-        throw new NotImplementedException();
-    }
-
-    [Test]
-    public void IncreaseSalary_NegativeIncrease_ShouldDecrease()
-    {
-        throw new NotImplementedException();
-    }
-
-    [Test]
-    public void IncreaseSalary_SmallerThanMinusTenPerc_ShouldFail()
-    {
-        throw new NotImplementedException();
+        [Test]
+        public void IncreaseSalary_SmallerThanMinusTenPerc_ShouldFail()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
